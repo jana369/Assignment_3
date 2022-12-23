7970c597668b983b33dcd6e0f7877233a6f799fc
jana369 <116430342+jana369@users.noreply.github.com>
2022-12-23 22:40:19 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<string>
+using namespace std;
+vector <string> sign = {"RED","GREEN"};
+vector <string> res;
+// Function to print the contents of the vector
+void printVector(vector<string> const& out)
+{
+    for (auto it = out.begin(); it != out.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+// Function to print all distinct combinations of length `k`, where the
+// repetition of elements is allowed
+void findCombinations(string arr[], vector<string>& out, int k, int i, int n)
+{
+    // base case: if the combination size is `k`, print it
+    if (out.size() == k)
+    {
+        printVector(out);
+        return;
+    }
+
+    // start from the previous element in the current combination
+    // till the last element
+    for (int j = 0; j < n; j++)
+    {
+        // add current element "arr[j]" to the solution and recur with the
+        // same index `j` (as repeated elements are allowed in combinations)
+        out.push_back(arr[j]);
+        findCombinations(arr, out, k, j, n);
+        // backtrack: remove the current element from the solution
+        out.pop_back();
+    }
+}
+
+int main()
+{
+    string arr[] = { "RED","GREEN","YELLOW"};
+    int k = 3;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    vector<string> out;
+    findCombinations(arr, out, k, 0, n);
+
+    return 0;
+}
