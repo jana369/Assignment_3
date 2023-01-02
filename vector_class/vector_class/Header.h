d5adddce97cc1ba37a52e2db3a95208bd0a54c6a
jana369 <116430342+jana369@users.noreply.github.com>
2023-01-02 18:32:08 +0200

    final_vision

--- vector_class/vector_class/Header.h
+++ vector_class/vector_class/Header.h
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<string>
 #include<iterator>
@@ -28,6 +27,9 @@ public:
 			++m_ptr;
 		}
 		return *this;
+	};
+	size_t operator+(const vector_iterator& other) {
+		return m_ptr + other.m_ptr;
 	};
 	vector_iterator& operator--() {//prefix operator
 		m_ptr--;
@@ -80,41 +82,45 @@ private:
 };
 template<class T>
 class AJVector {
+public :
+	using valueType = T;
+	using iterator = vector_iterator<AJVector<T>>;
+
 private:
- 	size_t size=0, capacity=0;
+	size_t size=0, capacity=0;
 	T* data=nullptr;
 public:
-    //constructors and big 4
-    AJVector(int capac)//creates a vector of size 0 and capacity == capac
-    {
-        this->size = 0;
-        this->capacity = capac;
-        data = new T[capacity];
-    };
+	//constructors and big 4
+	AJVector(size_t capac)//creates a vector of size 0 and capacity == capac
+	{
+		this->size = 0;
+		this->capacity = capac;
+		data = new T[capacity];
+	};
 
-    AJVector(int n, T *item) {//creates a vecor of array elements of size n
-        this->size = n;
-        this->capacity = n;
-        data = new T[capacity];
-        for (int i = 0; i < n; i++) {
-            data[i] = item[i];
-        }
-    };
+	AJVector(size_t n , T* item) {//creates a vecor of array elements of size n
+		this->size = n;
+		this->capacity = n;
+		data = new T[capacity];
+		for (int i = 0; i < n; i++) {
+			data[i] = item[i];
+		}
+	};
 
-    AJVector(const AJVector &other) : capacity(other.capacity) {//copy constructor
-        cout << "copy constructor";
-        this->size = other.size;
-        data = new T[capacity + 1];
-        for (int i = 0; i < size; i++) {
-            data[i] = other.data[i];
-        }
-    }
-
-    ~AJVector() {//to delete the dynamic storage
-        delete[] data;
-    }
-
- 	T& operator=(const AJVector& other) {//copy assignment
+	AJVector(const AJVector& other) :capacity(other.capacity) {//copy constructor
+		cout << "copy constructor";
+		this->size = other.size;
+		data = new T[capacity + 1];
+		for (int i = 0; i < size ; i++)
+		{
+			data[i] = other.data[i];
+		}
+	};
+	
+	~AJVector() {//to delete the dynamic storage
+		delete[] data;
+	};
+	T& operator=(const AJVector& other) {//copy assignment
 		if (data != &other.data) {
 			delete[] data;
 			size = other.size;
@@ -140,26 +146,21 @@ public:
 			other.size = 0;
 			other.capacity = 2;
 		}
-		return data;
+		return *data;
 	};
-
-    //access operations
 	//access operations
-	T& operator[](int index) {
-		try {
-			if (index >= 0 || index < size) {
-				return data[index];
+	T& operator[](size_t index) {
+		if(index<0 && index>=size){
+			try {
+					throw throwError();
 			}
-			else {
-				throw throwError();
+			catch (AJVector::throwError) {
+				cout << "Exception handling: the index is out_of_range";
 			}
 		}
-		catch (AJVector::throwError) {
-			cout << "Exception handling: the index is out_of_range";
-		}
+			return data[index];
 	};
-
-    //modifying operators
+	//modifying operators
 	size_t push_back(T new_item) {//to add a new data
 		if (size < capacity) {
 			data[size++] = new_item;
@@ -180,23 +181,20 @@ public:
 		}
 	};
 
-    size_t Size() const {
-        return size;
-    }
-
-    T pop_back() {
-
-        data[size - 1] = 0;
-        size--;
-    }
-
-    void clear() {
-        size = 0;
-        data = 0;
-    }
-    //void insert(iterator , T);
-    ////iterators
-    iterator begin() {
+	size_t Size() const {
+		return size;
+	};
+	T pop_back() {
+		size--;
+		return data[size];
+	};
+	void clear() {
+		delete[] data;
+		capacity = 2;
+		size = 0;
+		data = new T[capacity];
+	};
+	iterator begin() {
 		return iterator(data);
 	}
 	iterator end() {
@@ -224,82 +222,123 @@ public:
 		catch (AJVector::throwError) {
 			cout << "Exception Handling: access_denied"<<endl;
 		}
-		
 	};
-    
-     T begin() {
-        T *ptr = &data[0];
-        return *ptr;
+	void erase(iterator it1, iterator it2) {
+		try {
+			if (((it1 > begin() || it1 == begin()) && it1 < end()) && ((it2 > begin() || it2 == begin()) && it2 < end()) && (it2>it1))
+			{
+				T* new_one;
+				size_t s = size - (it2 - it1);
+				new_one = new T[(s)];
+				for (int i = 0; i < s ; i++) {
+					if (i < (it1 - begin())) {
+					new_one[i] = data[i];
+					}
+					else if (i >= (it1 - begin())) {
+					new_one[i] = data[i+(it2-it1)];
+					}
+				}
+				while (it2-it1)
+				{
+					size--;
+					capacity--;
+					it1++;
+				}
+				data = new_one;
+				new_one = nullptr;
+			}
+			else {
+				throw throwError();
+			}
+		}
+		catch (AJVector::throwError) {
+			cout << "Exception Handling: access_denied" << endl;
+		}
+	};
 
-    }
+	
 
-    T end() {
-        T *ptr = &data[size - 1];
-        return *ptr;
-    }
+	void insert(iterator it, T item) {
+		try {
+			if ((it > begin() || it == begin()) && it < end())
+			{
+				T* new_one;
+				new_one = new T[size + 1];
+				for (size_t i = 0; i < (it-begin()); i++) {
+					new_one[i] = data[i]; 
+				}
+				new_one[it - begin()] = item;
+				for (size_t i = it-begin()+1 ; i < size+1; i++) {
+					new_one[i] = data[i-1] ;
+				}
+				size++;
+				data = new_one;
+				new_one = nullptr;
+			}
+			else {
+				throw throwError();
+			}
+		}
+		catch (AJVector::throwError) {
+			cout << "Exception Handling: access_denied" << endl;
+		}
+	};
+	 	
+	//iterators
+	////comparison operators
+	bool operator==(const AJVector<T>& other) {
+		if (other.size == size) {
+			return true;
+		}
+		else
+			return false;
+	}
 
-    ////comparison operators
-    bool operator==(const AJVector<T> &other) {
-        if (other.size == size) {
-            return true;
-        } else
-            return false;
-    }
+	bool operator<(const AJVector<T>& other) {
+		if (size < other.size) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
-    bool operator<(const AJVector<T> &other) {
-        if (size < other.size) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	////capacity operations
+	size_t pCapacity() const {
+		return capacity;
+	}
 
-    ////capacity operations
-    int pCapacity() const {
-        return capacity;
+	void resize(int num) const {
+		T* new_vector = new T* { capacity };
+		for (int i = 0; i < size; i++) {
+			new_vector[i] = data[i];
+		}
+		delete[] data;
+		data = new_vector;
+		new_vector = nullptr;
+		delete[] new_vector;
+	}
 
-    }
+	bool empty() {
+		if (size == 0) {
+			return true;
+		}
+		else
+			return false;
+	}
 
-        void resize(int num){
-        T* new_vector = new T{num};
-        for (int i = 0; i < size; i++) {
-            new_vector[i] = data[i];
-        }
-        delete[] data;
-        capacity = num;
-        data = new T{num};
-        data = new_vector;
-        new_vector = nullptr;
-        delete new_vector;
-    }
+	bool is_empty() {
+		return size == 0;
+	};
 
-
-    bool empty() {
-        if (size == 0) {
-            return true;
-        } else
-            return false;
-    }
-
-    //friends
-    friend ostream &operator<<(ostream &stream, const AJVector<T> &other) {
-        for (int i = 0; i < other.size; i++) {
-            cout << other.data[i];
-        }
-        return stream;
-    }
+	//friends
+	friend ostream& operator << (ostream& ,const AJVector<T>&);
 	class throwError {};
 };
-
-int main(){
-    AJVector<int>vec{5};
-    vec.push_back(20);
-    vec.push_back(56);
-    vec.push_back(23);
-    cout << vec[0] << endl;
-    cout << vec.pCapacity() << " " << vec.Size() << endl;
-    cout << vec.begin() << endl;
-    cout << vec.end() << endl;
-    vec.clear();
-    cout << vec[2];
-}
+template<class T>
+ostream& operator<< (ostream& stream,const AJVector<T>& other) {
+	for (int i = 0; i < other.size; i++) {
+		cout << other.data[i];
+	}
+	return stream;
+};
